6705c5a97f9fbf1a966b7cacf77d8a0533776472
chishenglqh <86618712+chishenglqh@users.noreply.github.com>
2023-03-15 10:13:02 +0800

    Update csgo.sh

--- csgo/csgo.sh
+++ csgo/csgo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v1/\?appid\=730\&key\=<your-key>\&steamid\=<your-steam-id>
+curl -s http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v1/\?appid\=730\&key\=992E3854AF196FF7B2C919FF5B18A86B\&steamid\=chishenglqh
